2177cd0f9b7f30cd3193e87fbc9f4b146aabfa7e
hien <hien>
2023-04-28 16:58:07 +0700

    add to cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -12,7 +12,7 @@ exports.getCart = async (req, res) => {
       },
       include: [
         {
-          model: Product, as: 'product', attributes: ['title']
+          model: Product, as: 'product', attributes: ['title', 'amount']
         },
       ]
     });
